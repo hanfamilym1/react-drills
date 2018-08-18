4d5a0ddf2c556843f1277b4c05444479f1b3f65d
hanfamilym1 <michael@hanfamily.com>
2018-08-18 15:21:14 -0600

    Finished App 1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import Input from './Input.js'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(props){
+    super(props);
+    this.state={
+      message: ''
+    }
   }
+
+  handleChange(value){
+    this.setState({
+      message: value
+    })
+  }
+  
+  render(){
+      return(
+        <div>
+          <input onChange={(e) => this.handleChange(e.target.value)}/>
+          <p>{this.state.message}</p>
+        </div>
+      )
+    }
+    
+  
 }
 
 export default App;
