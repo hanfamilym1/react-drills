d6ec082bc77a47c74ac55b600bdf811657b74074
hanfamilym1 <michael@hanfamily.com>
2018-08-20 17:04:31 -0600

    Finished App6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+  constructor(){
+    super();
+    this.state={
+        list: [],
+        input: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleAddTask = this.handleAddTask.bind(this)
 }
+
+handleChange(val){
+    this.setState({
+        input: val
+    })
+}
+
+handleAddTask(){
+    this.setState({
+        list: [...this.state.list, this.state.input],
+        input: ''
+    })
+}
+
+render(){
+   let list = this.state.list.map((element,index)=> {
+     return (
+       <Todo key={index} task={element}/>
+     )
+   })
+   
+    return(
+        <div className='App'>
+            <h1>My to-do list</h1>
+            <input 
+            value={this.state.input}
+            placeholder="Enter new task"
+            onChange={e=>this.handleChange(e.target.value)}
+            type="text"/>
+            <button
+            onClick={this.handleAddTask}>Add</button>
+            {list}
+        </div>
+    )
+}
+}
+
 
 export default App;
